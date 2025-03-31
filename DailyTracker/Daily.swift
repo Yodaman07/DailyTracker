56d2957421d214c7dac9db8877de2065d52aa0b9
Yodaman07 <ayaan.irshad@gmail.com>
2025-03-30 21:53:45 -0700

    Worked a lot on getting daily data to sync with json files

--- DailyTracker/Daily.swift
+++ DailyTracker/Daily.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct Daily: View {
-    @State var pref : [DailyItem] = LoadPreferences();
-    @State var test : Bool = false
+    @State var day : Day =  Day(date: Date.now, activities: [])
     var body: some View {
         //TODO Scrollable
         VStack{
-            List($pref){ $data in
+            List($day.activities){ $data in
                 HStack{
                     Text($data.activity.wrappedValue)
                         .font(.title3)
@@ -21,15 +20,29 @@ struct Daily: View {
                     
                     Spacer()
                     
-                    Toggle(isOn: $test){Text("Completed?")}
+                    Toggle(isOn: $data.complete){Text("Completed?")}
                         .padding(10)
+                        .onChange(of: data.complete, initial: false){
+                            SaveData(data: day)
+                        }
                 }
                     
                     
             }
         }.onAppear{
-            print(Date.now)
-            pref = LoadPreferences()
+            var allDayData: [Day] = LoadData()
+
+            var date = getFormattedDate()
+            //Code to run if we have to add a new day
+            if (!dayExists(days: allDayData, date: date)){
+                let dayData = Day(date: date, activities: LoadPreferences())
+                day = dayData //update view data
+                SaveData(data: dayData) //update saved data
+                
+            }else{ //Code to run if we are just loading the day we already have
+                day = getDay(days: allDayData, date: date) //update view data
+            }
+            
         } //load preferences every time view is loaded
     }
 }
@@ -37,6 +50,96 @@ struct Daily: View {
 struct Day: Codable{
     var date: Date
     var activities: [DailyItem] = LoadPreferences() //Need to associate these with the day
+}
+
+
+func SaveData(data: Day){ //saving data from scratch
+
+//    print(String(data: encodedData, encoding: .utf8)!)
+//    let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+//    let jsonURL = documentsDirectory.appendingPathComponent("Data.json")
+    var dataToEncode : [Day] = [data]
+    let jsonURL = URL.documentsDirectory.appendingPathComponent("Data.json")
+
+    if FileManager.default.fileExists(atPath: jsonURL.path){
+        //If the file exists, get all of its data, and add a new Day element
+        dataToEncode = LoadData()
+        //Need to remove the old element with the same name
+        if dayExists(days: dataToEncode, date: getFormattedDate()){
+            let index = getIndexToRemove(days: dataToEncode, date: getFormattedDate())
+            print("Updating data at index " + String(index) )
+            dataToEncode.remove(at: index)
+        }else{print(Date.now)}
+            
+        dataToEncode.append(data) //add a new day
+    }
+    
+    //Regardless of if the file exists or not, we want to write either just the one val or the updated list to the file
+    let enc = JSONEncoder()
+    enc.outputFormatting = .prettyPrinted
+    let encodedData = try! enc.encode(dataToEncode)
+    try! encodedData.write(to: jsonURL)
+}
+
+
+func LoadData() -> [Day] {
+    print("Loading Day Data")
+    let dec = JSONDecoder()
+
+    let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    let jsonURL = documentsDirectory.appendingPathComponent("Data.json")
+    
+    if FileManager.default.fileExists(atPath: jsonURL.path){
+        //Regular loading
+        let d = try! Data(contentsOf: jsonURL)
+        return try! dec.decode(Array<Day>.self, from: d)
+    }else {
+        //If we try to load and the file doesn't exist, try to initialize data first
+        print("Making File")
+        let d : Day = Day(date: Date.now, activities: LoadPreferences())
+        SaveData(data: d)
+        return [d]
+    }
+
+}
+
+func getDay(days: [Day], date: Date) -> Day!{//gets the day object associate with a specific date
+    for day in days{
+        if day.date == date{
+            return day
+        }
+    }
+    return nil
+}
+
+//Check if the day already exists
+func dayExists(days: [Day], date: Date) -> Bool{
+    for day in days{
+        if day.date == date{
+            return true
+        }
+    }
+    return false
+}
+
+func getIndexToRemove(days: [Day], date: Date) -> Int {
+    var index = 0
+    for day in days{
+        if day.date == date{
+            return index
+        }
+        index+=1
+    }
+    return index
+}
+
+
+func getFormattedDate() -> Date{
+    let df = DateFormatter()
+    df.dateFormat = "yyyy-MM-dd"
+    let date : Date = df.date(from: df.string(from: Date.now))! //format date
+    //https://www.swiftyplace.com/blog/swift-date-formatting-10-steps-guide
+    return date
 }
 
 #Preview {
